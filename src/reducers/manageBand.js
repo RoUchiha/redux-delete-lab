5181a1275f293ac936406099571cf213ea13db44
RoUchiha <swaggsikh@gmail.com>
2022-05-27 01:55:44 -0500

    commit tree 6d236864512c810baef59aad41a42b335f920927

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,19 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: [],
 }, action) {
   switch(action.type) {
     case "ADD_BAND":
-      return {...state, bands: state.bands.concat(action.name)}
+      const band = {
+        id: uuid(),
+        name: action.payload
+      };
+      return { ...state, bands: [...state.bands, band] };
+
+    case "DELETE_BAND":
+        const bands = state.bands.filter(band => band.id !== action.id)
+        return { bands }
     default:
       return state
   
